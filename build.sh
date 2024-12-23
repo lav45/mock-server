b1ed63d93480a228396cd3ea751734987e6ba2f9
lav45 <lav451@gmail.com>
2024-12-23 13:51:08 +0300

    composer update

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker build -f docker/base.Dockerfile -t mock-server-base .
+docker build --pull -f docker/base.Dockerfile -t mock-server-base .
 docker build -f docker/dev.Dockerfile -t mock-server-dev .
 docker build -f docker/prod.Dockerfile -t mock-server-prod .
