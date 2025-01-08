38e00b17eabaca586f11fbab60b66b12843873e6
lav45 <lav451@gmail.com>
2025-01-08 10:34:16 +0300

    clear

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ set -e
 docker build --pull -f docker/base.Dockerfile -t mock-server-base .
 docker build -f docker/dev.Dockerfile -t mock-server-dev .
 
-./composer install --optimize-autoloader --classmap-authoritative --prefer-dist --no-progress --no-dev --ansi
+./composer install --optimize-autoloader --classmap-authoritative --prefer-dist --no-progress --ansi
 
 docker build -f docker/prod.Dockerfile -t mock-server-prod .
-
-./composer install --optimize-autoloader --classmap-authoritative --prefer-dist --no-progress --ansi
