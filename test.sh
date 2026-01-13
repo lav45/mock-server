c968044f05024926e135da28a0952b0345120e7a
lav45 <lav451@gmail.com>
2026-01-14 00:37:37 +0300

    update test.sh

--- test.sh
+++ test.sh
@@ -1,42 +1,58 @@
 #!/usr/bin/env sh
 
+set -e
+
+RUNNING_PROCESS=$(
+  docker ps --filter name='(test_webhook_catcher|test_mock_server|test_runner)' --format "{{.Names}}"
+)
+
+if [ "$RUNNING_PROCESS" ] ; then
+  for PROCESS in $RUNNING_PROCESS ; do
+    docker kill "$PROCESS" > /dev/null
+  done
+fi
+
 getIp() {
-  docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1
+  docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$1"
 }
 
 WEBHOOK_CATCHER_PID=$(
   docker run --rm -d \
-    -v $(pwd)/vendor:/app/vendor \
-    -v $(pwd)/test/Functional/Server:/app/test/Functional/Server \
+    -v "$(pwd)"/vendor:/app/vendor \
+    -v "$(pwd)"/test/Functional/Server:/app/test/Functional/Server \
     -e PORT=80 \
     -e LOG_LEVEL=error \
+    --name test_webhook_catcher \
     mock-server:server test/Functional/Server/start
 )
 
-WEBHOOK_CATCHER_URL=http://$(getIp ${WEBHOOK_CATCHER_PID})
+WEBHOOK_CATCHER_URL=http://$(getIp "$WEBHOOK_CATCHER_PID")
 
 MOCK_SERVER_PID=$(
   docker run --rm -d \
-    -v $(pwd)/test/Functional/mocks:/app/mocks \
+    -v "$(pwd)"/vendor:/app/vendor \
+    -v "$(pwd)"/test/Functional/mocks:/app/mocks \
     -e PORT=80 \
     -e LOG_LEVEL=error \
     -e MOCKS_PATH=/app/mocks \
     -e DOMAIN=test.server.com \
-    -e WEBHOOK_CATCHER_URL=${WEBHOOK_CATCHER_URL} \
+    -e WEBHOOK_CATCHER_URL="$WEBHOOK_CATCHER_URL" \
     -e FILE_WATCH_TIMEOUT=0 \
-    mock-server:server bin/start
+    --name test_mock_server \
+    mock-server:server
 )
 
-MOCK_SERVER_URL=http://$(getIp ${MOCK_SERVER_PID})
+MOCK_SERVER_URL=http://$(getIp "$MOCK_SERVER_PID")
 
 docker run --rm -i \
-  -u $(id -u):$(id -g) \
-  -v $(pwd):/app \
-  -e MOCK_SERVER_URL=${MOCK_SERVER_URL} \
-  -e WEBHOOK_CATCHER_URL=${WEBHOOK_CATCHER_URL} \
+  -u "$(id -u):$(id -g)" \
+  -v "$(pwd)":/app \
+  -e MOCK_SERVER_URL="$MOCK_SERVER_URL" \
+  -e WEBHOOK_CATCHER_URL="$WEBHOOK_CATCHER_URL" \
   -e COMPOSER_HOME=/app/.cache/.composer \
   --entrypoint composer \
-  mock-server:tool $1
+  --name test_runner \
+  mock-server:tool "$1"
 
-docker stop ${WEBHOOK_CATCHER_PID}
-docker stop ${MOCK_SERVER_PID}
+docker stop test_mock_server > /dev/null
+docker stop test_webhook_catcher > /dev/null
