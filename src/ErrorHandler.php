9e9720a6e63de7358e4e73e74c929d77496c6816
lav45 <lav451@gmail.com>
2026-03-10 23:49:21 +0300

    Add ErrorHandler to response to the error in json format.

---
+++ src/ErrorHandler.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Lav45\MockServer;
+
+use Amp\Http\Server\Request;
+use Amp\Http\Server\Response;
+
+final readonly class ErrorHandler implements \Amp\Http\Server\ErrorHandler
+{
+    public function handleError(int $status, string|null $reason = null, Request|null $request = null): Response
+    {
+        $response = new Response(
+            headers: ['content-type' => 'application/json'],
+        );
+
+        $response->setStatus($status, $reason);
+
+        $body = [
+            'status' => $status,
+            'message' => $response->getReason(),
+        ];
+
+        $response->setBody(
+            \json_encode($body, JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR),
+        );
+
+        return $response;
+    }
+}
